8a6e735c29b2e533f2c7ddf0b1212f2c0f212891
Billy Pinkerton <billybattlebones@gmail.com>
2022-04-02 21:24:02 -0700

    mind melting mess of fix attempts

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # code-quiz
+
+# This was another very challenging project, i couldnt get a reset function placed into the code and I aslo couldnt figure out how to link to another page. instead just to have a working display I placed the score on into a container on the page. I also could not get the reset button to work whatsoever. I removed it from the document and im going to have to come back to that.
+
+## The intended function of this project is to run a timed test of four questions or more using the time remaining at the end as the score. I was able to (with help of my teacher,tutor,TA and several other students) get the test starting. If you hit the green start button the timer starts and the questions are displayed. You can click on the questions and run through the test but it doesnt reset at the end. It does locally store the score and display it on page with the users initials. I will be coming back to the project to try and come up with the solutions.
+
+![alt text](./assets/Screenshot%20(72).png);
+![alt text](./assets/Screenshot%20(73).png);
+![alt text](./assets/Screenshot%20(74).png);
+![alt text](./assets/Screenshot%20(75).png);
--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var riddleArray = [
     {
-        question:"What is the first number in an array",
-        choices:["zero","one"],
-        answer:"zero",
+        question:'What is the first number in an array',
+        choices:['zero','one'],
+        answer:'zero',
     },
     {
-        question:"Booleans only have two values.",
+        question:'Booleans only have two values.',
         choices: ['True','False'],
         answer: 'True',
     },{
-        question:"Which of the following is not a JSON value?",
+        question:'Which of the following is not a JSON value?',
         choices: ['String','Number','Object','Function'],
         answer: 'Function',
     },{
-        question:"How do I return the absolute value of x?",
+        question:'How do I return the absolute value of x?',
         choices: ['log(x)','floor(x)','abs(x)','acos(x)'],
         answer: 'abs(x)',
     },
@@ -26,11 +26,9 @@ var timerCountDown = riddleArray.length *15;
 var timerInterval;
 var selectedRiddleIndex = 0;
 var containerTwo = document.getElementsByClassName('container-two');
-
 var containerFour = document.getElementById('score-tracker');
 
-/*to pull score?*/
-var scoreBoard = document.querySelector('#score-tracker');
+
 
 
 
@@ -38,32 +36,28 @@ var scoreBoard = document.querySelector('#score-tracker');
 
 /*game end function to stop after the fourth question*/
 function gameEnds(){
-    riddleContainer.innerHTML ="End";
-    scoreKeep();
+    riddleContainer.innerHTML ='End';
     clearInterval(timerInterval);
-    window.prompt(timerCountDown,"Save your initials");
+    var newIni = window.prompt(timerCountDown,'Save your initials');
+    scoreKeep(timerCountDown,newIni);
+    containerFour = document.getElementById('score-tracker');
+    containerFour.textContent = scoreGet();
 }
-
-    
-        
-
-
-
-
+/*to start the countdown timer*/
 function beginTime (){
     timerCountDown--;
     time.textContent = timerCountDown;
 }
-
+/*going through my riddle array and pulling each question to display on the page*/
 function questionGenerator(){
     if (selectedRiddleIndex === riddleArray.length){
         gameEnds();
     }
-    var riddle = document.createElement("p");
+    var riddle = document.createElement('p');
     riddle.textContent = riddleArray[selectedRiddleIndex].question;
-    var answerLog = document.createElement("ol");
+    var answerLog = document.createElement('ol');
     for (let index = 0; index < riddleArray[selectedRiddleIndex].choices.length; index++) {
-        var selectedAnswer = document.createElement("li");
+        var selectedAnswer = document.createElement('li');
         selectedAnswer.textContent = riddleArray[selectedRiddleIndex].choices[index];
         answerLog.append(selectedAnswer);
     }
@@ -73,20 +67,20 @@ riddleContainer.append(answerLog);
 /*This function is checking the answer and compairing it to the array to determin correct and incorrect answers*/
 function auditAnswer(event){
     var answerChoice = event.target.textContent;
-    var answeredCorrect = document.createElement("p");
+    var answeredCorrect = document.createElement('p');
     if (answerChoice === riddleArray[selectedRiddleIndex].answer){
-        answeredCorrect.textContent = ("Correct!")
+        answeredCorrect.textContent = ('Correct!')
         time.append(answeredCorrect);
     } else {
-        answeredCorrect.textContent = 'Incorrect!';
-        timerCountDown -= 0;
+        answeredCorrect.textContent = ('Incorrect!');
+        timerCountDown -= 15;
         time.append(answeredCorrect);
     }
     
 
 
 /*to clear the last question and setup the next.*/
-    riddleContainer.innerHTML = "";
+    riddleContainer.innerHTML = '';
     selectedRiddleIndex++;
     questionGenerator();
 
@@ -94,27 +88,33 @@ function auditAnswer(event){
 
 /*starts the quiz on the click of the button*/
 function quizStarter(){
-riddleBtn.classList.add("hide");
+riddleBtn.classList.add('hide');
 questionGenerator();
 timerInterval = setInterval(beginTime,1000);
 }
 
-function scoreKeep (){
-localStorage.setItem("score",timerCountDown);
+/*scoreKeep to store the score and scoreGet to pull the score*/
+function scoreKeep (newScore,newInitials){
+var oldScores = localStorage.getItem('score');
+if(!oldScores || oldScores.length === 0){
+    oldScores = '{}'
+}
+/*Trying to use JSON to hold the old scores and post them to the page.*/
+var jsonScores = JSON.parse(oldScores);
+jsonScores[newInitials] = newScore;
+localStorage.setItem('score',JSON.stringify(jsonScores));
+
+}
+/*need to pull the score data saved to local storage*/
+function scoreGet (){
+    var getScore = localStorage.getItem('score');
+    return getScore;
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
+/*listening for the clicks to start the game and check answers.*/
 riddleBtn.addEventListener('click', quizStarter);
-riddleContainer.addEventListener("click", auditAnswer);
+riddleContainer.addEventListener('click', auditAnswer);
+/*should I have an event listner to call the score function to display the score*/
